c7c58fd7dcd1e12ec0480a84fc1ab566d3fbba80
AMaskedDev <amaskeddev@gmail.com>
2024-06-10 15:59:45 +0300

    Update blacklist.lua

--- blacklist.lua
+++ blacklist.lua
@@ -1,4 +1,6 @@
-local blacklisted = {""}
+local plr = game:GetService("Players").LocalPlayer
+
+local blacklisted = {"SprouteRL"}
 if table.find(blacklisted, plr.Character.Name) then 
     plr.Backpack.ServerTraits.ChatAdvance:FireServer("Ban me")
     wait(0.1)
